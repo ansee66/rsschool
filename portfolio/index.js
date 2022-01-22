710551c677dc039ec948d2330efd487304ed8581
Natalya Solovyova <ns-66@yandex.ru>
2022-01-22 23:26:15 +0700

    feat: add adaptive menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -6,3 +6,17 @@ console.log(
   "20 из 20 - Сделана интерактивность, реализуемая через css"  + "\n" +
   "Итого: 100 баллов."
 )
+
+const nav = document.querySelector(".main-nav");
+const navToggle = document.querySelector(".main-nav__toggle");
+const navLinks = document.querySelectorAll(".main-nav__item");
+
+navToggle.addEventListener("click", function() {
+  nav.classList.toggle("open");
+});
+
+for (item of navLinks) {
+  item.addEventListener("click", function() {
+    nav.classList.remove("open");
+  })
+}
